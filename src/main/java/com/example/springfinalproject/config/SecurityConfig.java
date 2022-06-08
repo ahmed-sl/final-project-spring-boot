a960cc0ddb5db79f949d89d86ac38281878c6591
aalsu <aalsulami35@gmail.com>
2022-06-08 22:00:46 +0300

    remove security config to testin1

--- src/main/java/com/example/springfinalproject/config/SecurityConfig.java
+++ src/main/java/com/example/springfinalproject/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.example.springfinalproject.config;
 
-import com.example.springfinalproject.service.MyUserDetailsService;
+//import com.example.springfinalproject.service.MyUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
